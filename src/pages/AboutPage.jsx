7384ba55a6f7a49b57f2679d46e899a588a91108
daveSmithTeknic <dave_smith@teknic.com>
2023-01-06 16:18:21 -0500

    initial

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,19 @@
+import Card from "../components/shared/Card"
+import { Link } from "react-router-dom"
+
+function AboutPage() {
+  return (
+    <Card>
+        <div className="about">
+            <h1>About This Project</h1>
+            <p>This is a Reach app to leave feeback for a product or service</p>
+            <p>Version: 1.0.0</p>
+        </div>
+        <p>
+            <Link to="/">Back to Home</Link>
+        </p>
+    </Card>
+  )
+}
+
+export default AboutPage
